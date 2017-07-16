85207220b691823f7867100a092101c9b5132f36
Khanh Hua <giakhanh2487@gmail.com>
2017-07-16 22:55:03 +0800

    Improve performance buy using views with `keys`

--- src/lib/db.js
+++ src/lib/db.js
@@ -12,27 +12,38 @@ function dbGET (url, transformer=null) {
     if (transformer) {
       return transformer(rows);
     }
-    
+
+    return rows;
+  });
+}
+
+function dbPOST(url, data, transformer=null) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${btoa(CLOUDANT_KEY + ':' + CLOUDANT_PASSWORD)}`
+    },
+    body: JSON.stringify(data)
+  }).then(res => {
+    return res.json();
+  }).then(({rows}) => {
+    if (transformer) {
+      return transformer(rows);
+    }
+
     return rows;
   });
 }
 
 export function getAlbums () {
   const url = `https://fatmandesigner-blog.cloudant.com/hymnals/_design/album/_view/public-albums?limit=10&reduce=false`;
-  
+
   return dbGET(url, (data) => data.map(item => (item.value.id=item.value._id, item.value) ));
 }
 
 export function getSongsByIds (ids=[]) {
-  const urls = ids.map(id => `https://fatmandesigner-blog.cloudant.com/hymnals/${id}`);
-  
-  return Promise.all(urls.map(url => 
-    fetch(url, 
-      {
-        headers: {
-          'Authorization': `Basic ${btoa(CLOUDANT_KEY + ':' + CLOUDANT_PASSWORD)}`
-        }
-      }).then(res => {
-        return res.json().then(item => (item.id = item._id, item));
-      })))
+  const url = `https://fatmandesigner-blog.cloudant.com/hymnals/_design/song/_view/public-songs`;
+
+  return dbPOST(url, {keys: ids}, (data) => data.map(({value:item}) => (item.id=item._id, item) ));
 }
